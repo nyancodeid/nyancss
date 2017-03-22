88a6af5cef9d4ff64f939225c44dab0e22101462
Ryan Aunur Rassyid <ryandevstudio@gmail.com>
2017-03-22 13:15:58 +0700

    add gulp-banner

--- gulpfile.js
+++ gulpfile.js
@@ -7,15 +7,29 @@ var prefix = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
 var replace = require('gulp-string-replace');
 var pump = require('pump');
+var banner = require('gulp-banner');
+var pkg = require('./package.json');
 
 var path = {
 	css: ['css/nyancss.css'],
 	js: ['js/nyancss.js']
 };
 
+var comment = '/*\n' +
+    ' * <%= pkg.name %> <%= pkg.version %>\n' +
+    ' * <%= pkg.description %>\n' +
+    ' * <%= pkg.homepage %>\n' +
+    ' *\n' +
+    ' * Copyright 2017, <%= pkg.author %>\n' +
+    ' * Released under the <%= pkg.license %> license.\n' +
+    '*/\n\n';
+
 gulp.task('css', function() {
  return gulp.src(path.css)
  	.pipe(sourcemaps.init())
+ 	.pipe(banner(comment, {
+        pkg: pkg
+    }))
 	.pipe(prefix({
 	    browsers: ["> 0%"]
 	}))
@@ -31,6 +45,9 @@ gulp.task('js', function(cb) {
  pump([
  	gulp.src(path.js),
  	sourcemaps.init(),
+ 	banner(comment, {
+        pkg: pkg
+    }),
  	sourcemaps.write('.'),
 	gulp.dest('release/js/'),
 	rename("nyan.min.js"),
